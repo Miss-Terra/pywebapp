658a20f447d5c79f94e6fd10b31bef156ac9cddf
Miss-Terra <ms.terra.h@gmail.com>
2017-11-04 12:59:53 -0700

    startcontainer.sh

--- appspec/startcontainer.sh
+++ appspec/startcontainer.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 $(aws ecr get-login --region us-west-2)
+docker build -t miss-terra/pywebapp .
+docker tag miss-terra/pywebapp:latest 923010755332.dkr.ecr.us-west-2.amazonaws.com/miss-terra/pywebapp:latest
+docker push 923010755332.dkr.ecr.us-west-2.amazonaws.com/miss-terra/pywebapp:latest
 docker pull 923010755332.dkr.ecr.us-west-2.amazonaws.com/miss-terra/pywebapp:latest || {
     echo "ERROR: docker pull failed. Sleeping for 10 minutes to allow investigation..."
     sleep 600
